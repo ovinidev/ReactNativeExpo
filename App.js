617d4a9ec5385b063a3acbbe9c093cac1b24b402
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-09-28 12:22:01 -0300

    adding first compoentn

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <Cesta />
     </View>
   );
 }
